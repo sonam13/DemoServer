51a65ac6044080a5e9397096ce88ebdb53852e67
root <root@localhost.localdomain>
2017-06-08 16:33:55 +0530

    First commit

---
+++ src/main/java/com/tcs/Controller/DemoServerController.java
@@ -0,0 +1,13 @@
+package com.tcs.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class DemoServerController {
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public @ResponseBody String getMsg() {
+		return "Finally load balanced";
+	}
+}
